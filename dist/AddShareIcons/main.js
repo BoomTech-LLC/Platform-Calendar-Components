afa1d337d2ccfd484c867626ae670bdf4b2398ea
ZakharyanKar <ZakharyanK@boomte.ch>
2022-09-13 12:01:03 +0400

    fix: timeBox

---
+++ dist/AddShareIcons/main.js
@@ -0,0 +1,141 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = AddShareIcons;
+
+require("core-js/modules/web.dom-collections.iterator.js");
+
+var _react = _interopRequireWildcard(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _mainModule = _interopRequireDefault(require("./main.module.css"));
+
+require("../assets/styles/icons.css");
+
+var _constants = require("../helpers/constants");
+
+var _commons = require("../helpers/commons");
+
+var _addShare = require("../helpers/addShare");
+
+var _commonPropTypes = require("../helpers/commonPropTypes");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function AddShareIcons(_ref) {
+  let {
+    title = _constants.ADD_SHARE_ICONS_CONSTRUCTOR.TITLE,
+    comp_id,
+    instance,
+    titleBorderHidden = false,
+    addToSectionName = _constants.ADD_SHARE_ICONS_CONSTRUCTOR.ADD_TO_ICONS.addToSectionName,
+    hideAddToIcons = false,
+    shareSectionName = _constants.ADD_SHARE_ICONS_CONSTRUCTOR.SHARE_ICONS.shareSectionName,
+    hideShareIcons = false,
+    boomEventUrlBase,
+    event,
+    copyActionTooltipText = _constants.ADD_SHARE_ICONS_CONSTRUCTOR.SHARE_ICONS.copyActionTooltipText,
+    copiedTooltipText = _constants.ADD_SHARE_ICONS_CONSTRUCTOR.SHARE_ICONS.copiedTooltipText,
+    wrapperCustomClassNames = [],
+    titleCustomClassNames = [],
+    contentCustomClassNames = [],
+    copyTooltipCustomClassNames = [],
+    order = 'vertical'
+  } = _ref;
+  const [copyTooltipText, setCopyTooltipText] = (0, _react.useState)(copyActionTooltipText);
+  if (hideAddToIcons && hideShareIcons) return null;
+  return /*#__PURE__*/_react.default.createElement("div", {
+    className: (0, _commons.combineClassNames)([_mainModule.default.add_share_icons_block, _mainModule.default[order], ...wrapperCustomClassNames])
+  }, /*#__PURE__*/_react.default.createElement("h3", {
+    className: (0, _commons.combineClassNames)([titleBorderHidden ? '' : _mainModule.default.bordered, ...titleCustomClassNames])
+  }, title), /*#__PURE__*/_react.default.createElement("div", {
+    className: (0, _commons.combineClassNames)([_mainModule.default[order], ...contentCustomClassNames])
+  }, !hideAddToIcons && /*#__PURE__*/_react.default.createElement(AddShareIconsRow, {
+    constructor: _constants.ADD_SHARE_ICONS_CONSTRUCTOR.ADD_TO_ICONS,
+    sectionName: addToSectionName,
+    event: event,
+    rowId: _constants.ADD_SHARE_ICONS_CONSTRUCTOR.ADD_TO_ICONS.rowId
+  }), order === 'horizontal' && /*#__PURE__*/_react.default.createElement("div", {
+    className: _mainModule.default.horizontal_divider
+  }), !hideShareIcons && +event.kind !== 12 &&
+  /*#__PURE__*/
+  // this solution is temporary 
+  _react.default.createElement(AddShareIconsRow, {
+    comp_id: comp_id,
+    instance: instance,
+    sectionName: shareSectionName,
+    event: event,
+    boomEventUrlBase: boomEventUrlBase,
+    constructor: _constants.ADD_SHARE_ICONS_CONSTRUCTOR.SHARE_ICONS,
+    rowId: _constants.ADD_SHARE_ICONS_CONSTRUCTOR.SHARE_ICONS.rowId,
+    copyTooltipText: copyTooltipText,
+    setCopyTooltipText: setCopyTooltipText,
+    copiedTooltipText: copiedTooltipText,
+    copyActionTooltipText: copyActionTooltipText,
+    copyTooltipCustomClassNames: copyTooltipCustomClassNames
+  })));
+}
+
+const AddShareIconsRow = _ref2 => {
+  let {
+    comp_id,
+    constructor,
+    rowId,
+    sectionName,
+    event,
+    setCopyTooltipText,
+    boomEventUrlBase,
+    copiedTooltipText,
+    copyTooltipText,
+    copyActionTooltipText,
+    copyTooltipCustomClassNames
+  } = _ref2;
+  return /*#__PURE__*/_react.default.createElement("div", {
+    className: _mainModule.default.add_share_icons_row
+  }, /*#__PURE__*/_react.default.createElement("div", null, sectionName), /*#__PURE__*/_react.default.createElement("div", null, constructor.icons.map(btn => {
+    const isCopyLink = btn.type === 'copy';
+    return /*#__PURE__*/_react.default.createElement(_react.Fragment, {
+      key: "".concat(event.id, "-").concat(event.startDate, "-add-share-").concat(btn.type)
+    }, /*#__PURE__*/_react.default.createElement("button", {
+      className: (0, _commons.combineClassNames)(['icon-' + btn.type, isCopyLink ? _mainModule.default.hoverable : '']),
+      onMouseOut: () => isCopyLink && setTimeout(() => setCopyTooltipText(copyActionTooltipText), 300),
+      onClick: e => {
+        if (rowId === 1) return btn.clickHandler(e, btn.type, event);
+
+        if (rowId === 2) {
+          let eventUrl = (0, _addShare.generateEventUrl)(event, boomEventUrlBase, comp_id);
+          return isCopyLink ? btn.clickHandler(e, setCopyTooltipText, copiedTooltipText, eventUrl) : btn.clickHandler(e, btn.type, eventUrl);
+        }
+      }
+    }), isCopyLink && /*#__PURE__*/_react.default.createElement("span", {
+      className: (0, _commons.combineClassNames)([_mainModule.default.copy_tooltip, ...copyTooltipCustomClassNames])
+    }, copyTooltipText));
+  })));
+};
+
+AddShareIcons.propTypes = {
+  title: _propTypes.default.string,
+  comp_id: _propTypes.default.string.isRequired,
+  instance: _propTypes.default.string.isRequired,
+  titleBorderHidden: _propTypes.default.bool,
+  event: _propTypes.default.object.isRequired,
+  boomEventUrlBase: _propTypes.default.string.isRequired,
+  hideAddToIcons: _propTypes.default.bool,
+  addToSectionName: _propTypes.default.string,
+  hideShareIcons: _propTypes.default.bool,
+  shareSectionName: _propTypes.default.string,
+  copyActionTooltipText: _propTypes.default.string,
+  copiedTooltipText: _propTypes.default.string,
+  wrapperCustomClassNames: _commonPropTypes.PT_CLASSNAMES,
+  titleCustomClassNames: _commonPropTypes.PT_CLASSNAMES,
+  contentCustomClassNames: _commonPropTypes.PT_CLASSNAMES,
+  copyTooltipCustomClassNames: _commonPropTypes.PT_CLASSNAMES,
+  order: _propTypes.default.oneOf(['vertical', 'horizontal'])
+};
---
+++ dist/helpers/commonPropTypes.js
@@ -0,0 +1,260 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.SHAPE_EVENT = exports.SHAPE_REPEAT = exports.SHAPE_TICKETS = exports.SHAPE_TICKET = exports.TICKET_LIMITATION_TYPES = exports.TICKET_SALES_TYPES = exports.TICKET_PRICING_TYPES = exports.TICKET_TYPES = exports.CURRENCY_TYPES = exports.SHAPE_CURRENCY = exports.SHAPE_PLAN = exports.TICKET_BILLING_CYCLE_TYPES = exports.SHAPE_REGISTRATION = exports.REGISTRATION_COUNTDOWN_OPTIONS = exports.SHAPE_ORGANIZER = exports.SHAPE_LOCATION = exports.SHAPE_GUEST = exports.SHAPE_GUEST_TICKET = exports.SHAPE_PHYSICAL_LOCATION = exports.PT_UID = exports.PT_CID = exports.PT_CLASSNAMES = void 0;
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const PT_CLASSNAMES = _propTypes.default.arrayOf(_propTypes.default.string);
+
+exports.PT_CLASSNAMES = PT_CLASSNAMES;
+const PT_CID = _propTypes.default.string;
+exports.PT_CID = PT_CID;
+const PT_UID = _propTypes.default.string;
+exports.PT_UID = PT_UID;
+
+const SHAPE_PHYSICAL_LOCATION = _propTypes.default.shape({
+  address: _propTypes.default.string,
+  email: _propTypes.default.string,
+  name: _propTypes.default.string,
+  phone: _propTypes.default.string,
+  website: _propTypes.default.string,
+  city: _propTypes.default.string,
+  statesList: _propTypes.default.string,
+  country: _propTypes.default.string,
+  postal: _propTypes.default.string,
+  lat: _propTypes.default.number,
+  lng: _propTypes.default.number
+});
+
+exports.SHAPE_PHYSICAL_LOCATION = SHAPE_PHYSICAL_LOCATION;
+
+const SHAPE_GUEST_TICKET = _propTypes.default.shape({
+  label: _propTypes.default.string,
+  currency: _propTypes.default.string,
+  quantity: _propTypes.default.number,
+  price: _propTypes.default.number
+});
+
+exports.SHAPE_GUEST_TICKET = SHAPE_GUEST_TICKET;
+
+const SHAPE_GUEST = _propTypes.default.shape({
+  id: _propTypes.default.string,
+  date: _propTypes.default.string,
+  firstName: _propTypes.default.string,
+  lastName: _propTypes.default.string,
+  tickets: _propTypes.default.arrayOf(SHAPE_GUEST_TICKET)
+});
+
+exports.SHAPE_GUEST = SHAPE_GUEST;
+
+const SHAPE_LOCATION = _propTypes.default.shape({
+  type: _propTypes.default.oneOf(['physical', 'online', 'tbd']),
+  values: _propTypes.default.shape({
+    physical: SHAPE_PHYSICAL_LOCATION,
+    online: _propTypes.default.string,
+    tbd: _propTypes.default.string
+  })
+});
+
+exports.SHAPE_LOCATION = SHAPE_LOCATION;
+
+const SHAPE_ORGANIZER = _propTypes.default.shape({
+  email: _propTypes.default.string,
+  name: _propTypes.default.string,
+  phone: _propTypes.default.string,
+  website: _propTypes.default.string
+});
+
+exports.SHAPE_ORGANIZER = SHAPE_ORGANIZER;
+
+const REGISTRATION_COUNTDOWN_OPTIONS = _propTypes.default.oneOf(['15 mins', '30 mins', '45 mins', '60 mins']);
+
+exports.REGISTRATION_COUNTDOWN_OPTIONS = REGISTRATION_COUNTDOWN_OPTIONS;
+
+const SHAPE_REGISTRATION = _propTypes.default.shape({
+  cid: PT_CID,
+  enabled: _propTypes.default.bool,
+  general: _propTypes.default.shape({
+    admin: _propTypes.default.string,
+    external: _propTypes.default.bool,
+    url: _propTypes.default.string,
+    countDount: REGISTRATION_COUNTDOWN_OPTIONS
+  }),
+  guestsOptions: _propTypes.default.shape({
+    isLimited: _propTypes.default.bool,
+    limit: _propTypes.default.number,
+    show: _propTypes.default.bool
+  })
+});
+
+exports.SHAPE_REGISTRATION = SHAPE_REGISTRATION;
+const TICKET_BILLING_CYCLE_TYPES = ['Weekly', 'Monthly', 'Yearly'];
+exports.TICKET_BILLING_CYCLE_TYPES = TICKET_BILLING_CYCLE_TYPES;
+
+const SHAPE_PLAN = _propTypes.default.shape({
+  cycle: _propTypes.default.oneOf(TICKET_BILLING_CYCLE_TYPES),
+  price: _propTypes.default.number,
+  duration: _propTypes.default.number,
+  id: _propTypes.default.number
+});
+
+exports.SHAPE_PLAN = SHAPE_PLAN;
+
+const SHAPE_CURRENCY = _propTypes.default.shape({
+  id: _propTypes.default.number,
+  value: _propTypes.default.string,
+  code: _propTypes.default.string,
+  symbol: _propTypes.default.string
+});
+
+exports.SHAPE_CURRENCY = SHAPE_CURRENCY;
+const CURRENCY_TYPES = [{
+  id: 0,
+  value: '$ Dollars',
+  code: 'USD',
+  symbol: '$'
+}, {
+  id: 1,
+  value: '฿ Baht',
+  code: 'THB',
+  symbol: '฿'
+}, {
+  id: 2,
+  value: '€ Euros',
+  code: 'EUR',
+  symbol: '€'
+}, {
+  id: 3,
+  value: 'Ft Forint',
+  code: 'HUF',
+  symbol: 'Ft'
+}, {
+  id: 4,
+  value: 'CHF Francs',
+  code: 'CHF',
+  symbol: 'CHF'
+}, {
+  id: 5,
+  value: 'Kč Koruna',
+  code: 'CZK',
+  symbol: 'Kč'
+}, {
+  id: 6,
+  value: 'kr Krona',
+  code: 'SEK',
+  symbol: 'kr'
+}, {
+  id: 7,
+  value: '$ Mexican Pesos',
+  code: 'MXN',
+  symbol: '$'
+}, {
+  id: 8,
+  value: '£ Pounds Sterling',
+  code: 'GBP',
+  symbol: '£'
+}, {
+  id: 9,
+  value: 'RM Ringgit',
+  code: 'MYR',
+  symbol: 'RM'
+}, {
+  id: 10,
+  value: '₪ Shekel',
+  code: 'ILS',
+  symbol: '₪'
+}, {
+  id: 11,
+  value: 'zł Zloty',
+  code: 'PLN',
+  symbol: 'zł'
+}, {
+  id: 13,
+  value: '₹ Rupee',
+  code: 'INR',
+  symbol: '₹'
+}];
+exports.CURRENCY_TYPES = CURRENCY_TYPES;
+const TICKET_TYPES = ['Ticket', 'Plan', 'Donation'];
+exports.TICKET_TYPES = TICKET_TYPES;
+const TICKET_PRICING_TYPES = ['Paid', 'Free'];
+exports.TICKET_PRICING_TYPES = TICKET_PRICING_TYPES;
+const TICKET_SALES_TYPES = ['Dynamic', 'Fixed'];
+exports.TICKET_SALES_TYPES = TICKET_SALES_TYPES;
+const TICKET_LIMITATION_TYPES = ['Unlimited', 'Limited'];
+exports.TICKET_LIMITATION_TYPES = TICKET_LIMITATION_TYPES;
+
+const SHAPE_TICKET = _propTypes.default.shape({
+  id: _propTypes.default.number,
+  name: _propTypes.default.string,
+  type: _propTypes.default.oneOf(TICKET_TYPES),
+  price: _propTypes.default.shape({
+    type: _propTypes.default.oneOf(TICKET_PRICING_TYPES),
+    amount: _propTypes.default.number
+  }),
+  plans: _propTypes.default.arrayOf(SHAPE_PLAN),
+  sales: _propTypes.default.shape({
+    type: _propTypes.default.oneOf(TICKET_SALES_TYPES),
+    Dynamic: _propTypes.default.shape({
+      start: _propTypes.default.string
+    }),
+    Fixed: _propTypes.default.shape({
+      start: _propTypes.default.string,
+      end: _propTypes.default.string
+    })
+  }),
+  limit: _propTypes.default.shape({
+    type: _propTypes.default.oneOf(TICKET_LIMITATION_TYPES),
+    quantity: _propTypes.default.number,
+    show: _propTypes.default.bool,
+    perOrder: _propTypes.default.shape({
+      min: _propTypes.default.number,
+      max: _propTypes.default.number
+    })
+  }),
+  description: _propTypes.default.string
+});
+
+exports.SHAPE_TICKET = SHAPE_TICKET;
+
+const SHAPE_TICKETS = _propTypes.default.arrayOf(SHAPE_TICKET);
+
+exports.SHAPE_TICKETS = SHAPE_TICKETS;
+
+const SHAPE_REPEAT = _propTypes.default.shape({
+  type: _propTypes.default.oneOf(['Day', 'Week', 'Month', 'Year']),
+  interval: _propTypes.default.number,
+  end: _propTypes.default.oneOfType([_propTypes.default.string, _propTypes.default.number])
+});
+
+exports.SHAPE_REPEAT = SHAPE_REPEAT;
+
+const SHAPE_EVENT = _propTypes.default.shape({
+  id: _propTypes.default.number.isRequired,
+  key: _propTypes.default.string.isRequired,
+  kind: _propTypes.default.number,
+  title: _propTypes.default.string,
+  start: _propTypes.default.string,
+  allDay: _propTypes.default.bool,
+  startMs: _propTypes.default.number,
+  end: _propTypes.default.string,
+  endMs: _propTypes.default.number,
+  image: _propTypes.default.string,
+  desc: _propTypes.default.string,
+  color: _propTypes.default.string,
+  scheduledOn: _propTypes.default.string,
+  location: SHAPE_LOCATION,
+  organizer: SHAPE_ORGANIZER,
+  guests: _propTypes.default.arrayOf(SHAPE_GUEST),
+  registration: SHAPE_REGISTRATION,
+  ticketIds: _propTypes.default.arrayOf(_propTypes.default.number),
+  ticketEnabled: _propTypes.default.bool,
+  repeat: SHAPE_REPEAT
+});
+
+exports.SHAPE_EVENT = SHAPE_EVENT;
