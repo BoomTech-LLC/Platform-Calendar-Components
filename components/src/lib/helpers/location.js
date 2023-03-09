115e8e098e6201b8290ac740751c14eefe186504
NarekBoomTech <nareks@boomte.ch>
2023-03-09 13:05:44 +0400

    Removed lat lng in physical location"

--- components/src/lib/helpers/location.js
+++ components/src/lib/helpers/location.js
@@ -2,26 +2,27 @@ import { validateURL } from "./commons";
 import { LOCATION_TYPES } from "./constants";
 
 export function getLocationOptions(data, tbdText) {
+  const value = data.values[data.type];
 
-    const value = data.values[data.type]
-
-    switch (data.type) {
-        case LOCATION_TYPES.physical:
-            return {
-                isLink: data.values?.physical?.lat && data.values?.physical?.lng,
-                value: value.address,
-                href: `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(value.address)}`
-            }
-        case LOCATION_TYPES.online:
-            return {
-                isLink: true,
-                value,
-                href: validateURL(value)
-            }
-        default:
-            return {
-                isLink: false,
-                value: tbdText || value,
-            }
-    }
+  switch (data.type) {
+    case LOCATION_TYPES.physical:
+      return {
+        isLink: true,
+        value: value.address,
+        href: `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+          value.address
+        )}`,
+      };
+    case LOCATION_TYPES.online:
+      return {
+        isLink: true,
+        value,
+        href: validateURL(value),
+      };
+    default:
+      return {
+        isLink: false,
+        value: tbdText || value,
+      };
+  }
 }
